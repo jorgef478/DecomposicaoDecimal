d49d2afdb16b03580079efbe19839e402e1607b8
jorgef478 <e107ti@etec.sp.gov.br>
2023-09-15 20:46:11 -0300

    arquivo finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("+....Decomposição Decimal....+");
+Console.WriteLine();
+
+Console.Write("Digite um número inteiro.: ");
+int numerointeiro = Convert.ToInt32(Console.ReadLine());
+
+int restante = numerointeiro;
+
+int unidades = restante % 10;
+restante /= 10;
+
+int dezenas = restante % 10;
+restante /= 10;
+
+int centenas = restante;
+
+Console.WriteLine($"\nO número {numerointeiro} possui:");
+Console.WriteLine($" {unidades,10} unidade(s);");
+Console.WriteLine($" {dezenas,10} dezenas(s);");
+Console.WriteLine($" {centenas,10} centena(s).");
